88a0c1c53b1cdd416f8c8aa645bee57c0047e092
Donald Jung <121824517+donaldjung@users.noreply.github.com>
2024-11-30 15:09:35 -0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -71,11 +71,6 @@ function Header() {
               <a href="#talks">Talks</a>
             </li>
           )}
-          {viewResume && (
-            <li>
-              <a href="#resume">Resume</a>
-            </li>
-          )}
           <li>
             <a href="#contact">Contact Me</a>
           </li>
